2bb80ace2faf6a2f443395973b4283f79416e402
Nick Rashchuk <raschyk1993@gmail.com>
2025-11-12 18:54:41 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+})
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
